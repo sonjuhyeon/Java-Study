b242cb370581da8eb787dbe8c778433845882242
sonjuhyeon <ths120521113105@daum.net>
2025-04-02 23:58:33 +0900

    add java tutorial

---
+++ Java-start/src/tutorial/CommentJava.java
@@ -0,0 +1,12 @@
+package tutorial;
+
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // Hello Java1 출력
+        System.out.println("Hello Java2");
+        /* 여러줄 주석
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+         */
+    }
+}

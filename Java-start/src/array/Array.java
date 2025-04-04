5e38fb123244ba489d6fe88306a1453a7c0b9d75
sonjuhyeon <ths120521113105@daum.net>
2025-04-04 23:54:58 +0900

    배열

---
+++ Java-start/src/array/Array.java
@@ -0,0 +1,12 @@
+package array;
+
+public class Array {
+  public static void main(String[] args) {
+    int[] students;
+    students = new int[5];
+
+    for (int i = 0; i < students.length; i++) {
+      System.out.println(students[i]);
+    }
+  }
+}
